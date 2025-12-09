7e6aede06fc9fb68158d7a8a562151b4132307f4
BLightAdee <vassadamka2015@gmail.com>
2025-12-09 19:52:25 +0200

    Initial Commit

---
+++ TorzsekInC/TorzsekInCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorzsekInCApp.swift
+//  TorzsekInC
+//
+//  Created by BLightAdee on 09.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TorzsekInCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
